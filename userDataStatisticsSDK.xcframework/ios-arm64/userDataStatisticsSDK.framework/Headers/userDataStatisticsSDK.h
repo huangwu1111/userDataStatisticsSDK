2c4910dd0808a04fd292a98049fff66833d91f96
huangwu1111 <1243930473@qq.com>
2023-08-02 15:00:48 +0800

    fff

---
+++ userDataStatisticsSDK.xcframework/ios-arm64/userDataStatisticsSDK.framework/Headers/userDataStatisticsSDK.h
@@ -0,0 +1,18 @@
+//
+//  userDataStatisticsSDK.h
+//  userDataStatisticsSDK
+//
+//  Created by whqpMac007 on 2023/7/31.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for userDataStatisticsSDK.
+FOUNDATION_EXPORT double userDataStatisticsSDKVersionNumber;
+
+//! Project version string for userDataStatisticsSDK.
+FOUNDATION_EXPORT const unsigned char userDataStatisticsSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <userDataStatisticsSDK/PublicHeader.h>
+
+
